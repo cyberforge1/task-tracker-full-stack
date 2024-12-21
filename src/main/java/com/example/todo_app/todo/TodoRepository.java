331cf74532983486068b26d8b897fddc5f15ef2b
cyberforge1@gmail.com <cyberforge1@gmail.com>
2024-12-21 11:19:30 +1100

    project syncing

--- src/main/java/com/example/todo_app/todo/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-// TodoRepository.java
-
-package com.example.todo_app.todo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
